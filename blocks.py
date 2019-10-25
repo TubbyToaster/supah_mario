a97a66675a639ab5361617e13c6434f14e891ad1
jakobpatino <32020852+jakobpatino@users.noreply.github.com>
2019-10-25 11:01:54 -0700

    Update blocks.py

--- blocks.py
+++ blocks.py
@@ -3,14 +3,15 @@ from pygame.sprite import Sprite
 
 
 class Blocks(Sprite):
-    def __init__(self, ai_settings, screen):
+    def __init__(self, ai_settings, screen, image, x, y):
         super(Blocks, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
-        self.image = pygame.image.load('assets/block.bmp')
+        self.image = pygame.image.load(image)
         self.rect = self.image.get_rect()
-        self.rect.y = self.rect.height
-        self.x = float(self.rect.x)
+        self.rect.y = y
+        self.rect.x = x
+        self.x = float(x)
         self.image_index = 1
 
     def image_up(self):
