44bbb07595a91040bb49742d0959f3cc794158b8
jakobpatino <32020852+jakobpatino@users.noreply.github.com>
2019-10-25 11:05:21 -0700

    Update mario.py

--- mario.py
+++ mario.py
@@ -5,38 +5,46 @@ from pygame.math import Vector2
 
 
 class Mario(Sprite):
-    def __init__(self, ai_settings, screen, blocks):
+    def __init__(self, ai_settings, screen, g_blocks):
         super(Mario, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
-        self.image = pygame.image.load('assets/ship.bmp')
+        self.image = pygame.image.load('assets/mario/Rmario_stand.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        self.rect.bottom = self.screen_rect.bottom - 72
         self.center = float(self.rect.centerx)
         self.moving_right = False
         self.moving_left = False
         self.image_index = 1
         self.image_cap = 10
-        self.image_ = pygame.image.load('assets/ship.bmp')
+        #self.image_ = pygame.image.load('assets/ground_tiles/brick_1.bmp')
         self.spd = 5
-        self.vx = 0
+        # self.vx = 0
         self.vy = 0
         self.pos = 200
         self.jumping = False
         self.jumping_press = False
         self.change_x = 0
         self.change_y = 0
-        self.blocks = blocks
-        self.fric = 0
+        self.g_blocks = g_blocks
+        self.cap = 8
         self.jump_scaler = 0
         self.land = True
         self.inair = False
+        self.state = "idle"
+        self.fric = 0
+
+    def go_left(self):
+        self.change_x -= self.fric
+
+    def go_right(self):
+        self.change_x += self.fric
 
     def update(self):
-        if self.jumping:
-            self.change_y = -16
+        if self.jumping and self.change_y == 0:
+            self.change_y = -12
             self.jumping = False
         if self.jumping_press and self.jump_scaler == 0:
             if self.jump_scaler < 12:
@@ -47,45 +55,43 @@ class Mario(Sprite):
         else:
             self.jump_scaler = 0
 
+        ff = 1
+        if self.moving_right and self.fric < 8:
+            self.fric += ff
+        elif self.fric > 0:
+            self.fric -= ff
+        if self.moving_left and self.fric > -8:
+            self.fric -= ff
+        elif self.fric < 0:
+            self.fric += ff
+
+        self.change_x = self.fric
         self.calc_grav()
+        self.rect.x += self.change_x
+
+        for block in self.g_blocks:
+            if self.rect.colliderect(block.rect):
+                if self.change_x > 0 and self.rect.bottom != block.rect.top:
+                    self.rect.right = block.rect.left
+                elif self.change_x < 0 and self.rect.bottom != block.rect.top:
+                    self.rect.left = block.rect.right
         self.rect.y += self.change_y
-        # self.rect.x += self.change_x
-
-        block_list = pygame.sprite.spritecollide(self, self.blocks, False)
-        for block in block_list:
-            if self.change_y > 0:
-                self.rect.bottom = block.rect.top
-            elif self.change_y < 0:
-                self.rect.top = block.rect.bottom
-            if self.moving_left:
-                self.rect.right = block.rect.left
-            elif self.moving_right:
-                self.rect.left = block.rect.right
-            self.change_y = 0
-
-        if self.moving_right and self.rect.right < self.screen_rect.right and self.fric < 8:
-            self.fric += .75
-        elif not self.moving_right and self.fric > 0:
-            self.fric -= .75
-
-        if self.moving_left and self.rect.left > 0 and self.fric > -8:
-            self.fric -= .75
-        elif not self.moving_right and self.fric < 0:
-            self.fric += .75
-
-        if self.fric != 0:
-            self.center += self.fric
-
-        self.rect.centerx = self.center
+        for block in self.g_blocks:
+            if self.rect.colliderect(block.rect):
+                if self.change_y > 0:
+                    self.rect.bottom = block.rect.top
+                elif self.change_y < 0:
+                    self.rect.top = block.rect.bottom
+                self.change_y = 0
 
     def calc_grav(self):
         if self.change_y == 0:
-            self.change_y = 1.5
+            self.change_y = 1
         else:
-            self.change_y += 1.5
-        if self.rect.y >= 600 - self.rect.height and self.change_y >= 0:
+            self.change_y += 1
+        if self.rect.y >= 720 - self.rect.height and self.change_y >= 0:
             self.change_y = 0
-            self.rect.y = 600 - self.rect.height
+            self.rect.y = 720 - self.rect.height
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
