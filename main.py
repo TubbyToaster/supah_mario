f49fabbb37ad007ae484ca637372af2367e40db9
jakobpatino <32020852+jakobpatino@users.noreply.github.com>
2019-10-25 15:00:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -38,11 +38,30 @@ def run_game():
         "000000000000000",
         "000000000000000",
         "111111111111111",
-        "111111111111111",
-
+        "111111111111111"
     ]
-    test = Chunk(ai_settings, screen, g_blocks, bg_blocks, a1)
-    test.gen(0, "g")
+
+    a2 = [
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000023",
+        "000000000000054",
+        "111111111111111",
+        "111111111111111"
+    ]
+    g1_1_1 = Chunk(ai_settings, screen, g_blocks, bg_blocks, a1)
+    g1_1_2 = Chunk(ai_settings, screen, g_blocks, bg_blocks, a2)
+    g1_1_1.gen(0, "g")
+    g1_1_2.gen(720, "g")
 
     b1 = [
         "bbbbbbbbbbbbbbb",
@@ -59,14 +78,33 @@ def run_game():
         "bgkibbbbbbbbbbb",
         "gkjlibbbbbbmnnn",
         "aaaaaaaaaaaaaaa",
-        "aaaaaaaaaaaaaaa",
-
+        "aaaaaaaaaaaaaaa"
     ]
-    test2 = Chunk(ai_settings, screen, g_blocks, bg_blocks, b1)
-    test2.gen(0, "bg")
+
+    b2 = [
+        "bbbbbbbbbbbbbbb",
+        "bbbbbbbbbbbbbbb",
+        "bbbbpqrbbbbbbbb",
+        "bbbbstubbbbbpqq",
+        "bbbbbbbbbbbbstt",
+        "bbbbbbbbbbbbbbb",
+        "bbbbbbbbbbbbbbb",
+        "bbbbbbbbbbbbbbb",
+        "bbbbbbbbbbbbbbb",
+        "bbbbbbbbbbbbbbb",
+        "bbbbbbbbbbbbbbb",
+        "bbhbbbbbbbbbbbb",
+        "ogkibbbbmnobbbb",
+        "aaaaaaaaaaaaaaa",
+        "aaaaaaaaaaaaaaa"
+    ]
+    bg1_1_1 = Chunk(ai_settings, screen, g_blocks, bg_blocks, b1)
+    bg1_1_2 = Chunk(ai_settings, screen, g_blocks, bg_blocks, b2)
+    bg1_1_1.gen(0, "bg")
+    bg1_1_2.gen(720, "bg")
     while True:
         gf.check_events(ai_settings, screen, mario)
-        mario.update()
+        mario.update(g_blocks, bg_blocks)
 
         gf.update_screen(ai_settings, screen, mario, g_blocks, bg_blocks)
         mainClock.tick(40)
