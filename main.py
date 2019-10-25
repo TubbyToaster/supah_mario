d2feb4a08c49d621632b56e6cda0f15694d91d66
jakobpatino <32020852+jakobpatino@users.noreply.github.com>
2019-10-25 11:04:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from mario import Mario
 from settings import Settings
 from blocks import Blocks
 import functions as gf
+from level_gen import Chunk
+
+mainClock = pygame.time.Clock()
 
 
 def run_game():
@@ -12,18 +15,61 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
 
-    blocks = Group()
-    mario = Mario(ai_settings, screen, blocks)
+    g_blocks = Group()
+    bg_blocks = Group()
+    mario = Mario(ai_settings, screen, g_blocks)
 
-    gf.create_blocks(ai_settings, screen, blocks, 500, 368+32, 400)
-    gf.create_blocks(ai_settings, screen, blocks, 500, 280, 400)
-    gf.create_blocks(ai_settings, screen, blocks, 500+32, 280, 400)
-    gf.create_blocks(ai_settings, screen, blocks, 500+64, 280, 400)
+    #gf.create_blocks(ai_settings, screen, blocks, 500, 368+32, 400)
+    #gf.create_blocks(ai_settings, screen, blocks, 500, 280, 400)
+    #gf.create_blocks(ai_settings, screen, blocks, 500+32, 280, 400)
+    #gf.create_blocks(ai_settings, screen, blocks, 500+64, 280, 400)
+    a1 = [
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "000000000000000",
+        "111111111111111",
+        "111111111111111",
+
+    ]
+    test = Chunk(ai_settings, screen, g_blocks, bg_blocks, a1)
+    test.gen(0, "g")
+
+    b1 = [
+        "bbbbbbbbbbbbbbb",
+        "bbbbbbbbbbbbbbb",
+        "bbbbbbbbbdbbbbb",
+        "bbbbbbbbcefbbbb",
+        "bbbbbbbbbbbbbbb",
+        "bbbbbbbbbbbbbbb",
+        "bbbbbbbbbbbbbbb",
+        "bbbbbbbbbbbbbbb",
+        "bbbbbbbbbbbbbbb",
+        "bbbbbbbbbbbbbbb",
+        "bbhbbbbbbbbbbbb",
+        "bgkibbbbbbbbbbb",
+        "gkjlibbbbbbmnnn",
+        "aaaaaaaaaaaaaaa",
+        "aaaaaaaaaaaaaaa",
+
+    ]
+    test2 = Chunk(ai_settings, screen, g_blocks, bg_blocks, b1)
+    test2.gen(0, "bg")
     while True:
         gf.check_events(ai_settings, screen, mario)
         mario.update()
 
-        gf.update_screen(ai_settings, screen, mario, blocks)
+        gf.update_screen(ai_settings, screen, mario, g_blocks, bg_blocks)
+        mainClock.tick(40)
 
 
 run_game()
