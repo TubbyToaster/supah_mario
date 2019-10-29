f90ae87eeb6fd30cdac3c47f0b78870e2bf28ddc
TubbyToaster <dylanconnors@csu.fullerton.edu>
2019-10-28 21:10:15 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,10 +2,10 @@ import sys
 import pygame
 from blocks import Blocks
 from enemy import Enemy
-import random
+from items import Items
 
 
-def check_keyup_events(event, mario, g_blocks, bg_blocks):
+def check_keyup_events(ai_settings, screen, event, mario, g_blocks, bg_blocks, items):
     if event.key == pygame.K_RIGHT:
         mario.mov_right = False
         mario.change_x = 0
@@ -14,17 +14,26 @@ def check_keyup_events(event, mario, g_blocks, bg_blocks):
         mario.change_x = 0
     if event.key == pygame.K_UP:
         mario.jumping_press = False
+    if event.key == pygame.K_SPACE and mario.state == "fire":
+        mario.fireball()
+        if mario.dir_face == "left":
+            create_item(ai_settings, screen, g_blocks, bg_blocks, mario, items, "fireball", float(mario.rect.x)+5,
+                        mario.rect.bottom,
+                        float(mario.rect.x+10), True, False)
+        if mario.dir_face == "right":
+            create_item(ai_settings, screen, g_blocks, bg_blocks, mario, items, "fireball", float(mario.rect.x)+50,
+                        mario.rect.bottom,
+                        float(mario.rect.x+40), False, True)
 
 
 def check_keydown_events(event, mario):
-    if event.key == pygame.K_RIGHT:
+    if event.key == pygame.K_RIGHT and not mario.died:
         mario.mov_right = True
-        mario.go_right()
-    elif event.key == pygame.K_LEFT:
+    elif event.key == pygame.K_LEFT and not mario.died:
         mario.mov_left = True
         mario.go_left()
 
-    if event.key == pygame.K_UP and mario.landed == True:
+    if event.key == pygame.K_UP and mario.landed == True and not mario.died:
         mario.landed = False
         if not mario.jumping:
             mario.jumping = True
@@ -41,7 +50,6 @@ def create_g_blocks(ai_settings, screen, image, g_blocks, rx, ry, xx, type_):
     g_blocks.add(block)
 
 
-
 def create_bg_blocks(ai_settings, screen, image, bg_blocks, rx, ry, xx, type_):
     block = Blocks(ai_settings, screen, image, rx, ry, 0, type_)
     block.rect.y = ry
@@ -50,24 +58,30 @@ def create_bg_blocks(ai_settings, screen, image, bg_blocks, rx, ry, xx, type_):
     bg_blocks.add(block)
 
 
-def create_enemy(ai_settings, screen, g_blocks, bg_blocks, mario, enemies, type, rcenter, bottom, center, ):
+def create_enemy(ai_settings, screen, g_blocks, bg_blocks, mario, enemies, type, rcenter, bottom, center):
     enemy = Enemy(ai_settings, screen, g_blocks, bg_blocks, mario, type, rcenter, bottom, center)
     enemies.add(enemy)
 
 
-def check_events(ai_settings, screen, mario, g_blocks, bg_blocks, monitor):
+def create_item(ai_settings, screen, g_blocks, bg_blocks, mario, items, type, rcenter, bottom, center, mov_left,
+                mov_right):
+    item = Items(ai_settings, screen, g_blocks, bg_blocks, mario, type, rcenter, bottom, center, mov_left, mov_right)
+    items.add(item)
+
+
+def check_events(ai_settings, screen, mario, g_blocks, bg_blocks, monitor, items):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             check_keydown_events(event, mario)
         elif event.type == pygame.KEYUP:
-            check_keyup_events(event, mario, g_blocks, bg_blocks)
+            check_keyup_events(ai_settings, screen, event, mario, g_blocks, bg_blocks, items)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
 
 
-def update_screen(ai_settings, screen, mario, g_blocks, bg_blocks, enemies, monitor, chunks):
+def update_screen(ai_settings, screen, mario, g_blocks, bg_blocks, enemies, monitor, chunks, items):
     screen.fill(ai_settings.bg_color)
 
     monitor.update()
@@ -95,11 +109,16 @@ def update_screen(ai_settings, screen, mario, g_blocks, bg_blocks, enemies, moni
         if chunk.check_edge < 1:
             chunks.remove(chunk)
 
-
     for enemy in enemies:
         enemy.blitme()
         enemy.update()
+
+    for item in items:
+        item.blitme()
+        item.update()
+
     mario.blitme()
 
     pygame.display.flip()
+
 
