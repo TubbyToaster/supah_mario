673a8490a1fe9cd694eb7dd0fa8852d3bf50d7bb
jakobpatino <32020852+jakobpatino@users.noreply.github.com>
2019-10-29 09:11:10 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,3 @@
-import random
-
-
 class Settings:
     def __init__(self):
         self.screen_width = 1440
@@ -13,6 +10,22 @@ class Settings:
         self.frame = 0
         self.frame_end = 20
         self.level = 1
+        self.time = 400
+        self.time_count = 0
+        self.coins = 0
 
     def get_frame(self):
         return self.frame
+
+    def reset_scores(self):
+        self.time = 400
+        self.coins = 0
+        self.level = 1
+        self.high_score = 0
+        self.mario_lives = 3
+
+    def update_time(self):
+        self.time_count += 1
+        if self.time_count > 40 and self.time != 0:
+            self.time_count = 0
+            self.time -= 1
