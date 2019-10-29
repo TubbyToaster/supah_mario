9a41e1bd73156ae4d03a62477e2cfef916cf23ad
TubbyToaster <dylanconnors@csu.fullerton.edu>
2019-10-29 14:20:49 -0700

    update

--- functions.py
+++ functions.py
@@ -14,6 +14,10 @@ def check_keyup_events(ai_settings, screen, event, mario, g_blocks, bg_blocks, i
         mario.change_x = 0
     if event.key == pygame.K_UP:
         mario.jumping_press = False
+    if event.key == pygame.K_DOWN:
+        mario.crouch = False
+        if mario.state == "super" or mario.state == "fire":
+            mario.grow_up('assets/mario/Rsmario_stand.bmp')
     if event.key == pygame.K_SPACE and mario.state == "fire":
         mario.fireball()
         if mario.dir_face == "left":
@@ -32,6 +36,8 @@ def check_keydown_events(event, mario):
     elif event.key == pygame.K_LEFT and not mario.died:
         mario.mov_left = True
         mario.go_left()
+    elif event.key == pygame.K_DOWN and not mario.died:
+        mario.crouch = True
 
     if event.key == pygame.K_UP and mario.landed == True and not mario.died:
         mario.landed = False
@@ -58,8 +64,8 @@ def create_bg_blocks(ai_settings, screen, image, bg_blocks, rx, ry, xx, type_):
     bg_blocks.add(block)
 
 
-def create_enemy(ai_settings, screen, g_blocks, bg_blocks, enemies, type, rcenter, bottom, center):
-    enemy = Enemy(ai_settings, screen, g_blocks, bg_blocks, enemies, type, rcenter, bottom, center)
+def create_enemy(ai_settings, screen, g_blocks, bg_blocks, mario, enemies, type, rcenter, bottom, center, items):
+    enemy = Enemy(ai_settings, screen, g_blocks, bg_blocks, mario, type, rcenter, bottom, center, items)
     enemies.add(enemy)
 
 
@@ -68,6 +74,9 @@ def create_item(ai_settings, screen, g_blocks, bg_blocks, mario, items, type, rc
     item = Items(ai_settings, screen, g_blocks, bg_blocks, mario, type, rcenter, bottom, center, mov_left, mov_right)
     items.add(item)
 
+def check_time(ai_settings, scores):
+    ai_settings.update_time()
+    scores.prep_time()
 
 def check_events(ai_settings, screen, mario, g_blocks, bg_blocks, monitor, items):
     for event in pygame.event.get():
@@ -81,7 +90,7 @@ def check_events(ai_settings, screen, mario, g_blocks, bg_blocks, monitor, items
             mouse_x, mouse_y = pygame.mouse.get_pos()
 
 
-def update_screen(ai_settings, screen, mario, g_blocks, bg_blocks, enemies, monitor, chunks, items):
+def update_screen(ai_settings, screen, mario, g_blocks, bg_blocks, enemies, monitor, chunks, items, scores):
     screen.fill(ai_settings.bg_color)
 
     monitor.update()
@@ -119,6 +128,6 @@ def update_screen(ai_settings, screen, mario, g_blocks, bg_blocks, enemies, moni
 
     mario.blitme()
 
+    scores.show_score()
+
     pygame.display.flip()
-
-
