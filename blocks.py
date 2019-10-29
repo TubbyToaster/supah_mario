e744ce16ecfffeaf394ec7a029a956611a1a9bb2
TubbyToaster <dylanconnors@csu.fullerton.edu>
2019-10-28 21:09:40 -0700

    Update blocks.py

--- blocks.py
+++ blocks.py
@@ -1,5 +1,7 @@
 import pygame
 from pygame.sprite import Sprite
+import items
+import mario
 
 
 class Blocks(Sprite):
@@ -20,11 +22,15 @@ class Blocks(Sprite):
         self.ypos = y
         self.up_g = 0
         self.down_g = 0
+        self.dead = 0
 
     def image_up(self):
         self.image_index += 1
 
     def blitme(self):
+        if self.type_ == "bricks" and self.dead == 1:
+            self.kill()
+
         if self.up_g > 0:
             self.rect.y -= 2
             self.up_g -= 1
@@ -35,13 +41,25 @@ class Blocks(Sprite):
             self.up = False
             self.down_g = 5
 
-        self.screen.blit(self.image, self.rect)
+        if self.type_ != "hidden":
+            self.screen.blit(self.image, self.rect)
 
     def update_frame(self):
         if self.image_index == 1:
             self.image = self.image
 
     def blipup(self):
-        if self.up_g == 0 and self.down_g == 0:
+        if self.up_g == 0 and self.down_g == 0 and self.type_ != "reg":
             self.up_g = 5
             self.up = True
+
+            if self.type_ == "pup":
+                self.type_ = "v"
+                self.image =  pygame.image.load("assets/interactible/qblock_used_1.bmp")
+
+            if self.type_ == "qcoin":
+                self.type_ = "v"
+                self.image =  pygame.image.load("assets/interactible/qblock_used_1.bmp")
+
+            if self.type_ == "hidden" or self.type_ == "u":
+                self.type_ = "v"
