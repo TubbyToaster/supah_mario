da3c7919bb9e641691771f31f73f4c33eb86a352
TubbyToaster <dylanconnors@csu.fullerton.edu>
2019-10-29 14:24:16 -0700

    update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from level_gen import Chunk
 from level_monitor import Monitor
 from enemy import Enemy
 from blocks import Blocks
+from scores import Scoreboard
 import functions as gf
 
 mainClock = pygame.time.Clock()
@@ -16,6 +17,7 @@ def run_game():
     pygame.display.set_caption("Mario")
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    scores = Scoreboard(ai_settings, screen)
 
     g_blocks = Group()
     bg_blocks = Group()
@@ -23,22 +25,23 @@ def run_game():
     chunks = Group()
     items = Group()
 
-    monitor = Monitor(ai_settings, screen, g_blocks, bg_blocks, chunks)
+    monitor = Monitor(ai_settings, screen, g_blocks, bg_blocks, chunks, enemies, items)
     mario = Mario(ai_settings, screen, g_blocks, bg_blocks, enemies, monitor, chunks, items)
 
     #gf.create_enemy(ai_settings, screen, g_blocks, bg_blocks, mario, enemies, "reg", 500, 100, 500)
-    #gf.create_enemy(ai_settings, screen, g_blocks, bg_blocks, mario, enemies, "reg", 520, 400, 500)
+    gf.create_enemy(ai_settings, screen, g_blocks, bg_blocks, mario, enemies, "koopa", 620, 400, 500, items)
 
     gf.create_item(ai_settings, screen, g_blocks, bg_blocks, mario, items, "fireflower", 500, 400, 500, True, False)
 
     while True:
         gf.check_events(ai_settings, screen, mario, g_blocks, bg_blocks, monitor, items)
         mario.update()
-        enemies.upadte()
+        gf.check_time(ai_settings, scores)
 
-        gf.update_screen(ai_settings, screen, mario, g_blocks, bg_blocks, enemies, monitor, chunks, items)
+        gf.update_screen(ai_settings, screen, mario, g_blocks, bg_blocks, enemies, monitor, chunks, items, scores)
 
-        mainClock.tick(40)
+        mainClock.tick(120)
 
 
 run_game()
+
