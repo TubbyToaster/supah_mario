b126acf5d2f570d90cda7ca6a7d2513faea4fa45
TubbyToaster <dylanconnors@csu.fullerton.edu>
2019-10-29 22:26:19 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -22,11 +22,12 @@ def check_keyup_events(ai_settings, screen, event, mario, g_blocks, bg_blocks, i
         mario.fireball()
         if mario.dir_face == "left":
             create_item(ai_settings, screen, g_blocks, bg_blocks, mario, items, "fireball", float(mario.rect.x)+5,
-                        mario.rect.bottom,
+                        mario.rect.bottom-30,
                         float(mario.rect.x+10), True, False)
+
         if mario.dir_face == "right":
             create_item(ai_settings, screen, g_blocks, bg_blocks, mario, items, "fireball", float(mario.rect.x)+50,
-                        mario.rect.bottom,
+                        mario.rect.bottom-30,
                         float(mario.rect.x+40), False, True)
 
 
@@ -89,11 +90,43 @@ def check_events(ai_settings, screen, mario, g_blocks, bg_blocks, monitor, items
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
 
+def new_level(ai_settings, screen, mario, g_blocks, bg_blocks, enemies, monitor, chunks, items, scores):
+    mario.rect.centerx = mario.screen_rect.centerx - 200
+    mario.rect.bottom = mario.screen_rect.bottom - 200
+    mario.center = float(mario.rect.centerx)
+    mario.flag = False
+    for el in bg_blocks:
+        bg_blocks.remove(el)
+    for el in g_blocks:
+        g_blocks.remove(el)
+    for el in enemies:
+        enemies.remove(el)
+    for el in chunks:
+        chunks.remove(el)
+
+
 
 def update_screen(ai_settings, screen, mario, g_blocks, bg_blocks, enemies, monitor, chunks, items, scores):
     screen.fill(ai_settings.bg_color)
 
-    monitor.update()
+
+    if mario.state == "next!" and monitor.cur == 2:
+        new_level(ai_settings, screen, mario, g_blocks, bg_blocks, enemies, monitor, chunks, items, scores)
+        monitor.cur = 3
+
+    if mario.death == True and mario.rect.bottom > 1300:
+        for el in items:
+            items.remove(el)
+        mario.grow_up('assets/mario/Rmario_stand.bmp')
+        monitor.cur = 1
+        mario.death = False
+        mario.died = False
+        mario.death_blow = False
+        new_level(ai_settings, screen, mario, g_blocks, bg_blocks, enemies, monitor, chunks, items, scores)
+        mario.state = "reg"
+
+    monitor.update(monitor.level_list)
+
 
     for el in bg_blocks:
         el.blitme()
@@ -109,8 +142,6 @@ def update_screen(ai_settings, screen, mario, g_blocks, bg_blocks, enemies, moni
         el.update_frame()
         if el.rect.x < -60:
             g_blocks.remove(el)
-            monitor.pos += 1
-            print(monitor.pos)
         if el.image_index > 3:
             g_blocks.remove(el)
 
